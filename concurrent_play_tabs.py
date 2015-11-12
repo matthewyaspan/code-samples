be9cf391cbbd4940600a4a063aa42a0a997240ce
unknown <matthew.yaspan@gmail.com>
2015-11-12 14:15:36 -0500

    updated concurrent, node-example

--- concurrent_play_tabs.py
+++ concurrent_play_tabs.py
@@ -1,8 +1,5 @@
 # Matthew Yaspan
-
-# Read in a file each one line containing name of audio file (string note)
-# and then a string of tabs to simulate a guiar string
-
+# Rock Band Assignment
 
 import time
 import pyglet
@@ -10,40 +7,99 @@ import sys
 import threading
 from pyglet import media
 
-#num_threads = 0
-#mutex = Semaphore(1);
-#turnstile = Semaphore(0);
+numThreads = 0
+threadCount = 0
+bootstrap = 0
+numThreadsMutex = threading.Semaphore(1)
+playMutex = threading.Semaphore(1)
+mutex = threading.Semaphore(1)
+turnstile_1 = threading.Semaphore(0)
+turnstile_2 = threading.Semaphore(1)
 
-def playInstrument(audio, pitches):  
-  count = [0]
-  clock = pyglet.clock
-  clock.schedule_interval(playNote, .2, audio, pitches, count)
+def arrive(): 
+  global mutex
+  global threadCount
+  global turnstile_1
+  global turnstile_2
+  global numThreads
+  mutex.acquire()
+  threadCount += 1
+  if threadCount == numThreads:
+    turnstile_2.acquire()
+    turnstile_1.release()
+  mutex.release()
+  return
 
-def playNote(dt, audio, pitches, count):
-  if count[0] >= len(pitches):
-    return 0
-  sound = pyglet.media.load(audio, streaming=False)
+def waitToPlay():
+  global mutex
+  global threadCount
+  global turnstile_1
+  global turnstile_2
+  global numThreads
+  turnstile_1.acquire()
+  turnstile_1.release()
+
+def waitForFinish():
+  global mutex
+  global threadCount
+  global turnstile_1
+  global turnstile_2
+  global numThreads
+
+  mutex.acquire()
+  threadCount -= 1
+  if threadCount == 0:
+    turnstile_1.acquire()
+    turnstile_2.release()
+  mutex.release()
+  turnstile_2.acquire()
+  turnstile_2.release()
+
+def playInstrument(audio, pitches):
+  global numThreads
+  # arrive()
+  #waitToPlay()
+  #waitForFinish()
+
   player = media.Player()
-  if pitches[count[0]].isdigit():
-    print pitches[count[0]]
-    pitch = pitches[count[0]]
-    player.next_source()
-    player.pitch = 2.0**(float(pitch)/12.0)
-    player.queue(sound);
-    player.play(); 
-  # player.pause()
-  count[0] += 1
+  count = [0]
+  while count[0] < len(pitches) :
+    arrive()
+    waitToPlay()
+    playNote(audio, pitches, count, player)
+    if count[0] == len(pitches):
+      numThreadsMutex.acquire()
+      numThreads -= 1
+      numThreadsMutex.release()
+    waitForFinish()
   return 0
 
+def playNote( audio, pitches, count, player):
+  sound = pyglet.media.load(audio, streaming=False)
+  if pitches[count[0]].isdigit():
+    pitch = pitches[count[0]]
+    player.next_source()
+    player.queue(sound)
+    player.pitch = 2.0**(float(pitch)/12.0)
+    #playMutex.acquire()
+    player.play()
+    #playMutex.release()
+    time.sleep(0.2)
+    #player.pause()
+  else:
+    time.sleep(0.2)
+  count[0] += 1
+  return
+
 def main(argv):
-  
+  global threadCount
+  global numThreads 
   files  = []
   tabs = []
   players = []
   threads = []
   tab_file = open(argv[1])
   lines = tab_file.readlines()
-  print "num lines is: " + str(len(lines))
 
   for line in lines:
     temp =line.split(' ')
@@ -51,15 +107,17 @@ def main(argv):
       files.append(temp[0])
       tabs.append(temp[1])
     
-  i = 0
-  for tab in tabs:
+  for i in range(0, len(tabs)):
     threads.append(threading.Thread(target=playInstrument,
                    args=[files[i], tabs[i]]))
-    i += 1
+    numThreads += 1;
 
   for thread in threads:
     thread.start()
-  pyglet.app.run()
+   
+  for thread in threads:
+    thread.join()
+
 
 if __name__ == "__main__":
  main(sys.argv)
